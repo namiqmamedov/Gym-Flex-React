d1cc9b049c558409eddf3aca72151d4aa391dfcb
namiqmamedov <namigmm@code.edu.az>
2023-03-25 17:19:50 +0400

    refactor: Updated header & main section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,21 +3,38 @@ import Header from '../Header/Header'
 import './Hero.scss'
 
 const Hero = () => {
-  return (
-    <div className='hero'>
-        <div className='left-h'>
-            <Header/>  
+    return (
+        <div className='hero'>
+            <div className='left-h'>
+                <Header/>
 
-            <div className='the-best-ad'>
-              <div></div>
-              <span>the best fitness club in the town</span>
+                <div className='the-best-ad'>
+                    <div></div>
+                    <span>the best fitness club in the town</span>
+                </div>
+
+                <div className='hero-text'>
+                    <div>
+                        <span className='stroke-text'>Shape</span>
+                        <span>Your</span>
+                    </div>
+                    <div>
+                        <span>Ideal Body</span>
+                    </div>
+                    <div>
+                        <span>
+                            In here we will help you to shape and build your ideal body and live up your
+                            life to fullest
+                        </span>
+                    </div>
+                </div>
+
+            </div>
+            <div className='right-h'>
+                right side
             </div>
         </div>
-        <div className='right-h'>
-            right side
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Hero
