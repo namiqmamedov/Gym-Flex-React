6eaf356d9c5e7207526e29e736f716e6088b31a4
namiqmamedov <namigmm@code.edu.az>
2023-03-26 03:07:01 +0400

    Added emailjs in join component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Hero from './components/Hero/Hero'
+import Join from './components/Join/Join'
 import Plans from './components/Plans/Plans'
 import Programs from './components/Programs/Programs'
 import Reasons from './components/Reasons/Reasons'
@@ -15,6 +16,7 @@ function App() {
                 <Reasons/>
                 <Plans/>
                 <Testimonials/>
+                <Join/>
         </div>
     )
 }
