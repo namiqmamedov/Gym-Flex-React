dc1e577e13d532e7be28b0b8606c9a0ad4a25deb
namiqmamedov <namigmm@code.edu.az>
2023-03-25 22:49:36 +0400

    refactor: Updated reasons component

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -5,26 +5,41 @@ import image2 from '../../assets/image2.png'
 import image3 from '../../assets/image3.png'
 import image4 from '../../assets/image4.png'
 
-
 const Reasons = () => {
-  return (
-    <div className="Reasons" id="reasons">
-        <div className="left-r">
-            <img src={image1} alt="" />
-            <img src={image2} alt="" />
-            <img src={image3} alt="" />
-            <img src={image4} alt="" />
-        </div>
-        <div className="right-r">
-            <span>some reasons</span>
+    return (
+        <div className="Reasons" id="reasons">
+            <div className="left-r">
+                <img src={image1} alt=""/>
+                <img src={image2} alt=""/>
+                <img src={image3} alt=""/>
+                <img src={image4} alt=""/>
+            </div>
+            <div className="right-r">
+                <span>some reasons</span>
 
-            <div>
-                <span className='stroke-text'>why</span>
-                <span>choose us?</span>
+                <div>
+                    <div>
+                        <span className='stroke-text'>why</span>
+                        <span>choose us?</span>
+                    </div>
+                    <div>
+                        <img src="" alt=""></img>
+                        <span></span>
+                    </div>
+                    <div><img src="" alt=""/>
+                        <span></span>
+                    </div>
+                    <div><img src="" alt=""/>
+                        <span></span>
+                    </div>
+                    <div><img src="" alt=""/>
+                        <span></span>
+                    </div>
+                </div>
+
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Reasons
