7e6391b4bc83438b4a4160928e7df9ec16989435
namiqmamedov <namigmm@code.edu.az>
2023-03-25 23:05:00 +0400

    refactor: Updated reasons component

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -4,6 +4,10 @@ import image1 from '../../assets/image1.png'
 import image2 from '../../assets/image2.png'
 import image3 from '../../assets/image3.png'
 import image4 from '../../assets/image4.png'
+import tick from '../../assets/tick.png'
+import nb from '../../assets/nb.png'
+import adidas from '../../assets/adidas.png'
+import nike from '../../assets/nike.png'
 
 const Reasons = () => {
     return (
@@ -18,25 +22,31 @@ const Reasons = () => {
                 <span>some reasons</span>
 
                 <div>
-                    <div>
-                        <span className='stroke-text'>why</span>
-                        <span>choose us?</span>
-                    </div>
-                    <div>
-                        <img src="" alt=""></img>
-                        <span></span>
-                    </div>
-                    <div><img src="" alt=""/>
-                        <span></span>
-                    </div>
-                    <div><img src="" alt=""/>
-                        <span></span>
-                    </div>
-                    <div><img src="" alt=""/>
-                        <span></span>
-                    </div>
+                    <span className='stroke-text'>why</span>
+                    <span>choose us?</span>
                 </div>
 
+                <div className='details-r'>
+                    <div>
+                        <img src={tick} alt=""></img>
+                        <span>OVER 140+ EXPERT COACHS</span>
+                    </div>
+                    <div><img src={tick} alt=""/>
+                        <span>TRAIN SMARTER AND FASTER THAN BEFORE</span>
+                    </div>
+                    <div><img src={tick} alt=""/>
+                        <span>1 FREE PROGRAM FOR NEW MEMBER</span>
+                    </div>
+                    <div><img src={tick} alt=""/>
+                        <span>RELIABLE PARTNERS</span>
+                    </div>
+                </div>
+                <span className='partner-title'>OUR PARTNERS</span>
+                <div className="partners">
+                    <img src={nb} alt=""/>
+                    <img src={adidas} alt=""/>
+                    <img src={nike} alt=""/>
+                </div>
             </div>
         </div>
     )
