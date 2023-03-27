f28f0783c46433a6f28e1dfb5a45c9427c9e85fc
namiqmamedov <namigmm@code.edu.az>
2023-03-27 21:05:26 +0400

    Updated deploy.yml

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Gym-Flex-React/"
+  mode: "development",
+  build: {
+    minify: false,
+  }
 })
